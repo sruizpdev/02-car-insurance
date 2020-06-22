0188977e920b4be54f4b42bc3f70cfae7db2b15c
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-06-22 20:06:36 +0200

    first styled component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ContainerHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+const TextHeader = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`;
+const Header = ({ title }) => {
+  return (
+    <ContainerHeader>
+      <TextHeader>{title}</TextHeader>
+    </ContainerHeader>
+  );
+};
+
+export default Header;
